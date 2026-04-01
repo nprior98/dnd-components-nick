7194cf61ece44a106930f356d0b001e0fa76652d
Mollianeta <mollianeta@gmail.com>
2026-03-31 23:14:05 -0600

    Added melee/ranged classification and started the page

--- src/components/handbook/Weapon.tsx
+++ src/components/handbook/Weapon.tsx
@@ -1,9 +1,32 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import weaponJson from "../../../data/weapons/srd-2024_sickle.json"
+import "../../../themes/V3/phb.standalone.css";
+import { useEffect, useState } from "react";
+
+const weapon = weaponJson;
+
+//True if the range is 0 or if it can be thrown
+const isMelee = weapon.range === 0 || weapon.properties.some(
+  weapon => weapon.property.name === "Thrown");
+
 
 export default function Weapon() {
+
+
   return (
-    <Container>
-      <main className="page tocDepthH3" id="p1" data-index="0"></main>
-    </Container>
+    <div className="phb page" id="p3" data-index="2">
+      <h1>Weapon</h1>
+      <div>
+        <h2>{weapon.name}</h2>
+        <Container className="block frame wide">
+          <div>
+            Type: {weapon.is_simple ? "Simple" : "Martial"} {isMelee ? "Melee" : "Ranged"} Weapon
+          </div>
+          <hr/>
+
+
+        </Container>
+      </div>
+    </div>
   );
 }
