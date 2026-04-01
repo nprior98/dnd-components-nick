e71518772f2a3ff1a1ac27dffa526bc17bf1212e
Mollianeta <mollianeta@gmail.com>
2026-04-01 00:17:11 -0600

    adjusted weapon-type logic and added more content and comments to the weapon page

--- src/components/handbook/Weapon.tsx
+++ src/components/handbook/Weapon.tsx
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
-import weaponJson from "../../../data/weapons/srd-2024_sickle.json"
+import weaponJson from "../../../data/weapons/srd-2024_dagger.json"
 import "../../../themes/V3/phb.standalone.css";
 import { useEffect, useState } from "react";
 
@@ -7,7 +7,11 @@ const weapon = weaponJson;
 
 //True if the range is 0 or if it can be thrown
 const isMelee = weapon.range === 0 || weapon.properties.some(
-  weapon => weapon.property.name === "Thrown");
+  properties => properties.property.name === "Thrown");
+
+// working on a constant with arrow functions that holds the mastery property so that it can be displayed later
+// const masteryProperty = weapon.properties.some(
+//   properties => properties.property.type === "Mastery")
 
 
 export default function Weapon() {
@@ -18,14 +22,34 @@ export default function Weapon() {
       <h1>Weapon</h1>
       <div>
         <h2>{weapon.name}</h2>
-        <Container className="block frame wide">
+        <div>
           <div>
-            Type: {weapon.is_simple ? "Simple" : "Martial"} {isMelee ? "Melee" : "Ranged"} Weapon
+            <strong>Type: </strong>{weapon.is_simple ? "Simple" : "Martial"} {isMelee ? "Melee" : "Ranged"} Weapon <br />
           </div>
-          <hr/>
-
-
-        </Container>
+          <br />
+          <div>
+            Proficiency with a {weapon.name} allows you to add your proficiency bonus to the attack roll for any attack you make with it.
+            {/* <hr /> */}
+            <br />
+            This weapon has the following mastery property. To use this property, you must have a feature that lets you use it.
+            <br />
+            {/* mastery property stuff goes here */}
+            <br />
+            {/* will find a way to neatly display the grid later */}
+            <Container>
+              <Row>
+                <Col xs={3}><strong>Name</strong></Col>
+                <Col xs={4}><strong>Damage</strong></Col>
+                <Col xs={5}><strong>Properties</strong></Col>
+              </Row>
+              <Row>
+                <Col xs={3}>{weapon.name}</Col>
+                <Col xs={4}>{weapon.damage_dice} {weapon.damage_type.name}</Col>
+                <Col xs={5}>{/* will figure out how to list all properties later*/}</Col>
+              </Row>
+            </Container>
+          </div>
+        </div>
       </div>
     </div>
   );
