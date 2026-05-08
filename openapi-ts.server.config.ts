b83f9915940cc60e88d1f58db15722f2852b1d73
Colin Hanway <chanway@hey.com>
2026-05-07 18:05:56 -0600

    remove generation for encounter api

--- openapi-ts.server.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "@hey-api/openapi-ts";
-import { openApiSpec } from "./server/openapi";
-
-export default defineConfig({
-  input: openApiSpec,
-  output: "src/modules/encounter-api/",
-  plugins: ["@hey-api/client-axios"],
-});
